10edc3b426d3ddef60b7b3b8c858db518692bbbb
YiMing Han <hanyiming1995@gmail.com>
2026-06-24 21:21:07 -0400

    setup

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["onsen_*"],
+} satisfies Config;
